37b48e1217c5d7f35c79e5676ed589dae4ec4dfe
BobSegrest <BobSegrest@users.noreply.github.com>
2022-11-03 08:39:42 -0400

    Coorrecting Pat out path in Linux

--- eHaWconfig.py
+++ eHaWconfig.py
@@ -17,7 +17,6 @@ from PyQt5.QtWidgets import (
 from eHaWconfig_ui import Ui_MainWindow
 eDict = {}
 
-
 class Window(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
